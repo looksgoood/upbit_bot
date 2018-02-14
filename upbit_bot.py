2084172b9f8bfa3c82a2524c69ff20b29d07b017
Jaehyun Cho <looksgoood12@gmail.com>
2018-02-15 00:24:21 +0900

    Add volumn check job.

--- upbit_bot.py
+++ upbit_bot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import threading
 import telegram
 import json
@@ -14,28 +16,35 @@ telegram_chatbot_id = ''
 request_user_agent = ''
 standard_price = {}
 
-state_standard = 5.0
+state_standard = 0.05
+volumn_standard = 0.5
 
 class CryptoCoin:
-    def __init__(self, openingPrice):
-            self.openingPrice = openingPrice
+    def __init__(self, prevClosingPrice):
+            self.prevClosingPrice = prevClosingPrice
             self.now_state = 1.0
+            self.is_volumn_checked = False
+
+def send_message_to_bot(massage):
+    print(massage)
+    bot = telegram.Bot(token = telegram_token)
+    bot.sendMessage(chat_id=telegram_chatbot_id, text=massage)
 
 def check_diff_trigger(coin_name, tradePrice):
-    openingPrice = standard_price[coin_name].openingPrice
+    prevClosingPrice = standard_price[coin_name].prevClosingPrice
     now_state = standard_price[coin_name].now_state
 
-    if (now_state + 0.05) * openingPrice < tradePrice:
-        standard_price[coin_name].now_state += 0.05
+    if (now_state + state_standard) * prevClosingPrice < tradePrice:
+        standard_price[coin_name].now_state += state_standard
         return True
-    elif (now_state - 0.05) * openingPrice > tradePrice:
-        standard_price[coin_name].now_state -= 0.05
+    elif (now_state - state_standard) * prevClosingPrice > tradePrice:
+        standard_price[coin_name].now_state -= state_standard
         return True
     
     return False
 
-def calculate_percent(openingPrice, tradePrice):
-    return (tradePrice - openingPrice) / openingPrice
+def calculate_percent(prevClosingPrice, tradePrice):
+    return (tradePrice - prevClosingPrice) / prevClosingPrice
 
 def upbit_api(period_type, period, market, coin, data_count, date):
     upbit_maked_url = ''
@@ -47,7 +56,7 @@ def upbit_api(period_type, period, market, coin, data_count, date):
     if date:
         upbit_maked_url = '%s&to=%s' % (upbit_maked_url, date)
 
-    print(upbit_maked_url)
+    # print(upbit_maked_url)
     headers = {'user-agent': request_user_agent}
     resp = requests.get(upbit_maked_url, headers=headers)
     result = json.loads(resp.text)
@@ -62,20 +71,24 @@ def get_current_market_price(coin):
 
     if coin not in standard_price:
         dic = upbit_api('days', None, 'KRW', coin, 1, None)
-        new_coin = CryptoCoin(dic[0]['openingPrice'])
+        new_coin = CryptoCoin(dic[0]['prevClosingPrice'])
         standard_price[coin] = new_coin
+
+    # reset volumn check flag
+    standard_price[coin].is_volumn_checked = False
 
     tradePrice = current_price['tradePrice']
     if check_diff_trigger(coin, tradePrice):
-        percent = calculate_percent(standard_price[coin].openingPrice, tradePrice)
+        percent = calculate_percent(standard_price[coin].prevClosingPrice, tradePrice)
         message = coin + ': %f (%.2f%%)' % (tradePrice, percent*100) + '\n'
         return True, message
 
     return False, ''
 
-def process_func():
+def price_check_func():
     now = time.localtime()
     s = "%04d-%02d-%02d %02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min)
+    print('====================================' + s + '==========================================')
     bot_message = s + '  current price\n'
 
     update_message = False
@@ -86,9 +99,7 @@ def process_func():
             update_message = True
 
     if update_message is True:
-        print(bot_message)
-        bot = telegram.Bot(token = telegram_token)
-        bot.sendMessage(chat_id=telegram_chatbot_id, text=bot_message)
+        send_message_to_bot(bot_message)
 
 def get_token_and_chat_id():
     f = open(privacy + '/token.txt', mode='r')
@@ -106,21 +117,58 @@ def get_token_and_chat_id():
     return token.strip(), chat_id.strip(), user_agent.strip()
 
 def new_day_setting_func():
+    print('new day setting!=============================================================')
     for coin in interest_coin_list:
         dic = upbit_api('days', None, 'KRW', coin, 1, None)
-        new_coin = CryptoCoin(dic[0]['openingPrice'])
+        new_coin = CryptoCoin(dic[0]['prevClosingPrice'])
+        print(coin, 'price is', new_coin.prevClosingPrice)
         standard_price[coin] = new_coin
+    print('new day setting!=============================================================')
+
+def get_current_market_volumn(coin):
+    if standard_price[coin].is_volumn_checked:
+        return False, ''
+
+    print('get', coin, 'current volumn.')
+    result = upbit_api('minutes', 10, 'KRW', coin, 2, None)
+    # print(result)
+    current_volumn = result[0]['candleAccTradeVolume']
+    prev_volumn = result[1]['candleAccTradeVolume']
+
+    if prev_volumn * (1.0 + volumn_standard) <= current_volumn:
+        percent = (current_volumn - prev_volumn) / prev_volumn
+        message = coin + ' volumn 급증! (%.2f%%)' % (percent*100) + '\n'
+        standard_price[coin].is_volumn_checked = True
+        return True, message
+
+    return False, ''
+
+def volumn_check_func():
+    bot_message = 'Volumn 급등!!!!!!!!!!!!!!\n'
+
+    update_message = False
+    for coin in interest_coin_list:
+        result, message = get_current_market_volumn(coin)
+        if result is True:
+            bot_message += message
+            update_message = True
+
+    if update_message is True:
+        send_message_to_bot(bot_message)
+
+
+
+
 
 if __name__ == '__main__':
-    # main()
     telegram_token, telegram_chatbot_id, request_user_agent = get_token_and_chat_id()
     new_day_setting_func()
-    process_func()
+    price_check_func()
     sched = BackgroundScheduler()
     sched.start()
-    sched.add_job(new_day_setting_func,'cron', hour='9')
-    sched.add_job(process_func,'cron', hour='0-23', minute='*/10')
-    # sched.add_job(process_func,'cron', hour='0-23', second='*/10')
+    sched.add_job(new_day_setting_func,'cron', hour='9', second='1')
+    sched.add_job(price_check_func,'cron', hour='0-23', minute='*/10', second='2')
+    sched.add_job(volumn_check_func,'cron', hour='0-23', minute='*/1,*/2,*/3,*/4,*/5,*/6,*/7,*/8,*/9')
 
     try:
         # This is here to simulate application activity (which keeps the main thread alive).
@@ -135,7 +183,7 @@ if __name__ == '__main__':
 #         pass
 
 #     def TaskA(self):
-#         process_func()
+#         price_check_func()
 #         threading.Timer(30,self.TaskA).start()
 
 #     def TaskB(self):
